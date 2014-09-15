6deb2b24c072889d685596bf2554657a0abc988d
Nikolay Labinskiy <e-moe@ukr.net>
2014-09-15 17:44:51 +0300

    docstrings

--- src/Levi9/SonataBundle/Service/CloneService.php
+++ src/Levi9/SonataBundle/Service/CloneService.php
@@ -4,6 +4,12 @@ namespace Levi9\SonataBundle\Service;
 
 class CloneService
 {
+    /**
+     * Clone object
+     *
+     * @param mixed $object Object instance to clone
+     * @return mixed Cloned $object instance
+     */
     public function cloneObject($object)
     {
         return clone $object;
